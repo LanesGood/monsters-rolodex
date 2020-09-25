b3408b243e403c769b0ae0f7bd07f627defc144c
Lane Goodman <Lane@developmentseed.org>
2020-09-25 13:54:54 -0400

    Updates

---
+++ precache-manifest.064f7968ba9d0306798950724d05ffc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56cf6a89c8d398a73ae789897415e3e8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cea9d0293cee065ebf52",
+    "url": "/monsters-rolodex/static/css/main.cf2c37d2.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cea9d0293cee065ebf52",
+    "url": "/monsters-rolodex/static/js/main.412c1f54.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
